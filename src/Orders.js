83cb76f972ea54f9d9abca4f88cac0aaf43ab495
vampire-yuta <yuta@vamdemicsystem.black>
2020-12-14 20:25:39 +0900

    update hooks

--- src/Orders.js
+++ src/Orders.js
@@ -7,6 +7,25 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
+import axios from 'axios';
+import {useState} from "react";
+
+function getDomains(){
+    const [result, setPod] = useState({ pods: [] });
+
+    const request = axios.create({
+    baseURL: "http://127.0.0.1:1323",
+    method: "GET"
+  })
+
+  request.get("/getdomains")
+      .then(request => {
+          setPod(result)
+        console.log(result)
+      })
+}
+
+const domains = getDomains()
 
 // Generate Order Data
 function createData(id, date, name, shipTo, paymentMethod, amount) {
@@ -14,39 +33,7 @@ function createData(id, date, name, shipTo, paymentMethod, amount) {
 }
 
 const rows = [
-  createData(
-    0,
-    '16 Mar, 2019',
-    'Elvis Presley',
-    'Tupelo, MS',
-    'VISA ⠀•••• 3719',
-    312.44,
-  ),
-  createData(
-    1,
-    '16 Mar, 2019',
-    'Paul McCartney',
-    'London, UK',
-    'VISA ⠀•••• 2574',
-    866.99,
-  ),
   createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(
-    3,
-    '16 Mar, 2019',
-    'Michael Jackson',
-    'Gary, IN',
-    'AMEX ⠀•••• 2000',
-    654.39,
-  ),
-  createData(
-    4,
-    '15 Mar, 2019',
-    'Bruce Springsteen',
-    'Long Branch, NJ',
-    'VISA ⠀•••• 5919',
-    212.79,
-  ),
 ];
 
 function preventDefault(event) {
@@ -75,13 +62,13 @@ export default function Orders() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
+          {domains.map((domain) => (
+            <TableRow key={domain.id}>
+              <TableCell>{domain.Name}</TableCell>
+              <TableCell>{domain.Name}</TableCell>
+              <TableCell>{domain.Name}</TableCell>
+              <TableCell>{domain.Name}</TableCell>
+              <TableCell align="right">{`$${domain.ID}`}</TableCell>
             </TableRow>
           ))}
         </TableBody>
